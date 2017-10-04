cc30158fc821e6dd58e8cea9a074c70513ed5d2a
adaw <adaw@localhost>
2017-10-04 19:30:01 +0200

    Versio 0.2 del projecte pr01_m8

--- pr01_m8/src/pr01_m8/Pr01_m8.java
+++ pr01_m8/src/pr01_m8/Pr01_m8.java
@@ -16,6 +16,8 @@ public class Pr01_m8 {
         " </head>");
         bw.newLine();
         bw.write(" <body>");
+        bw.write(" <h1>DAW2 m08uf4pr2</h1>");
+        bw.newLine();
         bw.newLine();
         bw.write(" Nova p&agrave;gina web");
         bw.newLine();
